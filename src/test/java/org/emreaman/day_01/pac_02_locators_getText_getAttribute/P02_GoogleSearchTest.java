9112bf46ff39d96b9f86d111432a1627cb779647
emreForte <emreaman@forte.io>
2024-05-22 01:10:08 +0300

    added trace view

--- src/test/java/org/emreaman/day_01/pac_02_locators_getText_getAttribute/P02_GoogleSearchTest.java
+++ src/test/java/org/emreaman/day_01/pac_02_locators_getText_getAttribute/P02_GoogleSearchTest.java
@@ -1,6 +1,8 @@
 package org.emreaman.day_01.pac_02_locators_getText_getAttribute;
 
 import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Browser.NewContextOptions;
+import com.microsoft.playwright.BrowserContext;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.BrowserType.LaunchOptions;
 import com.microsoft.playwright.Keyboard;
@@ -8,18 +10,35 @@ import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Locator.PressSequentiallyOptions;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.Tracing;
+import com.microsoft.playwright.Tracing.StopOptions;
+import io.qameta.allure.Allure;
+import java.io.FileInputStream;
+import java.nio.file.Paths;
+import org.junit.After;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class P02_GoogleSearchTest {
+  Playwright playwright;
+  BrowserType chromium;
+  Browser browser;
+  Page page;
+  BrowserContext context;
 
   @Test
   void test1() {
 
-    Playwright playwright = Playwright.create();
-    BrowserType chromium = playwright.chromium();
-    Browser browser = chromium.launch(new LaunchOptions().setHeadless(false));
-    Page page = browser.newPage();
+    playwright = Playwright.create();
+    chromium = playwright.chromium();
+    browser = chromium.launch(new LaunchOptions().setHeadless(false));
+    Browser.NewContextOptions newContextOptions = new NewContextOptions();
+    context = browser.newContext(newContextOptions);
+    context
+        .tracing()
+        .start(new Tracing.StartOptions().setScreenshots(true).setSnapshots(true).setSources(true));
+    page = browser.newPage();
 
     page.navigate("http://www.google.com");
 
@@ -39,6 +58,26 @@ public class P02_GoogleSearchTest {
     page.close();
     browser.close();
     playwright.close();
+  }
+
+  @After
+  public void after() {
+    try {
+
+        String tracingFilePath = "target/test.zip";
+        context.tracing()
+            .stop(new StopOptions().setPath(Paths.get(tracingFilePath)));
+        Allure.attachment("pw-trace", new FileInputStream(tracingFilePath));
+
+
+    } catch (Exception e) {
+
+    } finally {
+
+      page.close();
+      browser.close();
+      playwright.close();
+    }
   }
 
 }
