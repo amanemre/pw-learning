0d8ba37e1bfa254c07e0d21840ae835f6184b619
emreForte <emreaman@forte.io>
2024-05-22 11:55:43 +0300

    adjusted test steps

--- src/test/java/org/emreaman/day_01/pac_02_locators_getText_getAttribute/P02_GoogleSearchTest.java
+++ src/test/java/org/emreaman/day_01/pac_02_locators_getText_getAttribute/P02_GoogleSearchTest.java
@@ -16,20 +16,24 @@ import io.qameta.allure.Allure;
 import java.io.FileInputStream;
 import java.nio.file.Paths;
 import org.junit.After;
-import org.junit.jupiter.api.AfterAll;
+import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import io.qameta.allure.Step;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 
 public class P02_GoogleSearchTest {
+
   Playwright playwright;
   BrowserType chromium;
   Browser browser;
   Page page;
   BrowserContext context;
 
-  @Test
-  void test1() {
-
+  @Before
+  public void setup() {
     playwright = Playwright.create();
     chromium = playwright.chromium();
     browser = chromium.launch(new LaunchOptions().setHeadless(false));
@@ -39,41 +43,58 @@ public class P02_GoogleSearchTest {
         .tracing()
         .start(new Tracing.StartOptions().setScreenshots(true).setSnapshots(true).setSources(true));
     page = browser.newPage();
+  }
 
+
+  @Epic("Web interface")
+  @Feature("Essential features")
+  @Story("Navigate page")
+  @Test
+  void test() {
+    step1();
+    step2();
+    step3();
+    step4();
+  }
+
+  @Step("Step 1 - Navigate to page")
+  void step1() {
     page.navigate("http://www.google.com");
+  }
 
+  @Step("Step 2 - input search")
+  void step2() {
     Locator searchBoxElement = page.locator("textarea[name='q']");
 
-    //searchBoxElement.fill("Selenium");
     searchBoxElement.pressSequentially("Selenium", new PressSequentiallyOptions().setDelay(
         300)); // it writes given input like human pressing keys one by one
+  }
 
-    page.waitForTimeout(2000);
-
+  @Step("Step 3 - press enter")
+  void step3() {
     Keyboard keyboard = page.keyboard();
     keyboard.press("Enter");
-
     page.waitForTimeout(2000);
-    Assertions.assertEquals(1,1);
-    page.close();
-    browser.close();
-    playwright.close();
+  }
+
+  @Step("Step 4 - assert")
+  void step4() {
+    Assertions.assertEquals(1, 1);
   }
 
   @After
   public void after() {
     try {
 
-        String tracingFilePath = "target/test.zip";
-        context.tracing()
-            .stop(new StopOptions().setPath(Paths.get(tracingFilePath)));
-        Allure.attachment("pw-trace", new FileInputStream(tracingFilePath));
+      String tracingFilePath = "target/test.zip";
+      context.tracing()
+          .stop(new StopOptions().setPath(Paths.get(tracingFilePath)));
+      Allure.attachment("pw-trace", new FileInputStream(tracingFilePath));
 
 
     } catch (Exception e) {
 
     } finally {
-
       page.close();
       browser.close();
       playwright.close();
